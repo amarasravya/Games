877c4ffd9cfd90ae02ec6c40e79df3bfc5c6673e
Amara sravya <144253714+amarasravya@users.noreply.github.com>
2024-04-18 19:07:06 +0530

    Add files via upload

---
+++ sri2.js
@@ -0,0 +1,676 @@
+let questions = [
+    {
+      numb: 1,
+      question: "What does HTML stand for?",
+      answer: "Hyper Text Markup Language",
+      options: [
+        "Hyper Text Preprocessor",
+        "Hyper Text Markup Language",
+        "Hyper Text Multiple Language",
+        "Hyper Tool Multi Language"
+      ]
+    },
+    {
+      numb: 2,
+      question: "What does CSS stand for?",
+      answer: "Cascading Style Sheet",
+      options: [
+        "Common Style Sheet",
+        "Colorful Style Sheet",
+        "Computer Style Sheet",
+        "Cascading Style Sheet"
+      ]
+    },
+    {
+      numb: 3,
+      question: "What does PHP stand for?",
+      answer: "Hypertext Preprocessor",
+      options: [
+        "Hypertext Preprocessor",
+        "Hypertext Programming",
+        "Hypertext Preprogramming",
+        "Hometext Preprocessor"
+      ]
+    },
+    {
+      numb: 4,
+      question: "What does SQL stand for?",
+      answer: "Structured Query Language",
+      options: [
+        "Stylish Question Language",
+        "Stylesheet Query Language",
+        "Statement Question Language",
+        "Structured Query Language"
+      ]
+    },
+    {
+      numb: 5,
+      question: "What does XML stand for?",
+      answer: "eXtensible Markup Language",
+      options: [
+        "eXtensible Markup Language",
+        "eXecutable Multiple Language",
+        "eXTra Multi-Program Language",
+        "eXamine Multiple Language"
+      ]
+    },
+    {
+      numb: 6,
+      question: "What does HTTP stand for?",
+      answer: "Hypertext Transfer Protocol",
+      options: [
+        "Hypertext Transfer Protocol",
+        "Hypertext Transmission Protocol",
+        "Hyperlink Transfer Protocol",
+        "Hypertext Translation Protocol"
+      ]
+    },
+    {
+      numb: 7,
+      question: "What is the unit of measurement for force?",
+      answer: "Newton",
+      options: [
+        "Watt",
+        "Joule",
+        "Newton",
+        "Tesla"
+      ]
+    },
+    {
+      numb: 8,
+      question: "What is the SI unit of electric current?",
+      answer: "Ampere",
+      options: [
+        "Watt",
+        "Volt",
+        "Ampere",
+        "Ohm"
+      ]
+    },
+    {
+      numb: 9,
+      question: "What is the speed of light in vacuum?",
+      answer: "299,792,458 meters per second",
+      options: [
+        "299,792,458 meters per second",
+        "300,000,000 meters per second",
+        "200,000,000 meters per second",
+        "400,000,000 meters per second"
+      ]
+    },
+    {
+      numb: 10,
+      question: "Who proposed the theory of relativity?",
+      answer: "Albert Einstein",
+      options: [
+        "Isaac Newton",
+        "Albert Einstein",
+        "Stephen Hawking",
+        "Nikola Tesla"
+      ]
+    },
+    {
+      numb: 11,
+      question: "What is the SI unit of energy?",
+      answer: "Joule",
+      options: [
+        "Watt",
+        "Newton",
+        "Joule",
+        "Volt"
+      ]
+    },
+    {
+      numb: 12,
+      question: "What is the formula for calculating velocity?",
+      answer: "Velocity = Distance / Time",
+      options: [
+        "Velocity = Distance * Time",
+        "Velocity = Distance - Time",
+        "Velocity = Time / Distance",
+        "Velocity = Distance / Time"
+      ]
+    },
+    {
+      numb: 13,
+      question: "What is the law of conservation of energy?",
+      answer: "Energy cannot be created or destroyed, only transformed or transferred",
+      options: [
+        "Energy can be created and destroyed",
+        "Energy cannot be created or destroyed, only transformed or transferred",
+        "Energy can only be created, not destroyed",
+        "Energy can only be destroyed, not created"
+      ]
+    },
+    // Chemistry
+    {
+      numb: 14,
+      question: "What is the chemical symbol for gold?",
+      answer: "Au",
+      options: [
+        "Go",
+        "Ag",
+        "Au",
+        "Gd"
+      ]
+    },
+    {
+      numb: 15,
+      question: "What is the chemical formula for water?",
+      answer: "H2O",
+      options: [
+        "HO2",
+        "H2O",
+        "H3O",
+        "HO"
+      ]
+    },
+    {
+      numb: 16,
+      question: "What is the pH value of a neutral solution?",
+      answer: "7",
+      options: [
+        "0",
+        "14",
+        "7",
+        "10"
+      ]
+    },
+    {
+      numb: 17,
+      question: "What is the lightest element on the periodic table?",
+      answer: "Hydrogen",
+      options: [
+        "Helium",
+        "Oxygen",
+        "Carbon",
+        "Hydrogen"
+      ]
+    },
+    {
+      numb: 18,
+      question: "What is the chemical symbol for iron?",
+      answer: "Fe",
+      options: [
+        "Ir",
+        "Fe",
+        "In",
+        "Fr"
+      ]
+    },
+    {
+      numb: 19,
+      question: "What is the process of a substance changing from a solid to a gas without passing through the liquid phase called?",
+      answer: "Sublimation",
+      options: [
+        "Evaporation",
+        "Condensation",
+        "Sublimation",
+        "Freezing"
+      ]
+    },
+    {
+      numb: 20,
+      question: "What is the main component   of natural gas?",
+      answer: "Methane",
+      options: [
+        "Propane",
+        "Methane",
+        "Ethane",
+        "Butane"
+      ]
+    },
+    {
+      numb: 21,
+      question: "What is the chemical formula for table salt?",
+      answer: "NaCl",
+      options: [
+        "H2O",
+        "NaCl",
+        "CO2",
+        "NH3"
+      ]
+    },
+    {
+      numb: 22,
+      question: "What is the chemical symbol for lead?",
+      answer: "Pb",
+      options: [
+        "Pl",
+        "Le",
+        "Pb",
+        "Pd"
+      ]
+    },
+    {
+      numb: 23,
+      question: "What is the process of a substance changing from a gas to a liquid called?",
+      answer: "Condensation",
+      options: [
+        "Evaporation",
+        "Condensation",
+        "Sublimation",
+        "Deposition"
+      ]
+    },
+    {
+      numb: 24,
+      question: "Which gas is responsible for the greenhouse effect?",
+      answer: "Carbon dioxide",
+      options: [
+        "Oxygen",
+        "Nitrogen",
+        "Carbon dioxide",
+        "Methane"
+      ]
+    },
+    {
+      numb: 25,
+      question: "What is the most abundant element in the Earth's atmosphere?",
+      answer: "Nitrogen",
+      options: [
+        "Oxygen",
+        "Carbon",
+        "Helium",
+        "Nitrogen"
+      ]
+    },
+    {
+      numb: 26,
+      question: "What is the atomic number of carbon?",
+      answer: "6",
+      options: [
+        "8",
+        "12",
+        "6",
+        "14"
+      ]
+    },
+    {
+      numb: 27,
+      question: "What type of bond is formed when electrons are shared between atoms?",
+      answer: "Covalent bond",
+      options: [
+        "Ionic bond",
+        "Covalent bond",
+        "Metallic bond",
+        "Hydrogen bond"
+      ]
+    },
+    // Python
+    {
+      numb: 28,
+      question: "Which of the following is not a valid variable name in Python?",
+      answer: "2variable",
+      options: [
+        "my_variable",
+        "variable2",
+        "Variable_2",
+        "2variable"
+      ]
+    },
+    {
+      numb: 29,
+      question: "What is the result of the following expression? \n2 + 2 * 3",
+      answer: "8",
+      options: [
+        "12",
+        "6",
+        "8",
+        "10"
+      ]
+    },
+    {
+      numb: 30,
+      question: "Which keyword is used for ending the iteration in a loop prematurely in Python?",
+      answer: "break",
+      options: [
+        "next",
+        "return",
+        "stop",
+        "break"
+      ]
+    },
+    {
+      numb: 31,
+      question: "What will be the output of the following code? \nfor i in range(5): \n   print(i, end=' ')",
+      answer: "0 1 2 3 4",
+      options: [
+        "0 1 2 3 4",
+        "1 2 3 4 5",
+        "0 1 2 3",
+        "1 2 3 4"
+      ]
+    },
+    {
+      numb: 32,
+      question: "Which of the following data types is immutable in Python?",
+      answer: "Tuple",
+      options: [
+        "List",
+        "Dictionary",
+        "Tuple",
+        "Set"
+      ]
+    },
+    {
+      numb: 33,
+      question: "What does the 'elif' keyword in Python signify?",
+      answer: "It is used to check for multiple conditions after the 'if' statement.",
+      options: [
+        "It is used to execute a block of code if a condition is false.",
+        "It is used to define a block of code to be executed if a condition is true.",
+        "It is used to check for multiple conditions after the 'if' statement.",
+        "It is used to terminate a loop."
+      ]
+    },
+    {
+      numb: 34,
+      question: "What does the 'pass' statement do in Python?",
+      answer: "It is a null operation; nothing happens when it is executed.",
+      options: [
+        "It is used to terminate a loop.",
+        "It is used to define a block of code to be executed if a condition is true.",
+        "It is used to execute a block of code if a condition is false.",
+        "It is a null operation; nothing happens when it is executed."
+      ]
+    },
+    {
+      numb: 35,
+      question: "What is the purpose of the 'lambda' keyword in Python?",
+      answer: "It is used to create anonymous functions.",
+      options: [
+        "It is used to define a block of code to be executed if a condition is true.",
+        "It is used to terminate a loop.",
+        "It is used to create anonymous functions.",
+        "It is used to execute a block of code if a condition is false."
+      ]
+    },
+    {
+      numb: 36,
+      question: "What is the output of the following code snippet? \nnumbers = [1, 2, 3, 4, 5] \nprint(numbers[2:4])",
+      answer: "[3, 4]",
+      options: [
+        "[2, 3]",
+        "[1, 2]",
+        "[4, 5]",
+        "[3, 4]"
+      ]
+    },
+    {
+      numb: 37,
+      question: "What is the output of the following code? \nprint('Hello' + 3 * ' World')",
+      answer: "Hello World World World",
+      options: [
+        "Hello World",
+        "Hello World World",
+        "Hello World World World",
+        "Hello World World World World"
+      ]
+    },
+    {
+      numb: 38,
+      question: "Which of the following is a correct way to open a file in Python for reading?",
+      answer: "open('filename.txt', 'r')",
+      options: [
+        "open('filename.txt', 'w')",
+        "open('filename.txt', 'r')",
+        "open('filename.txt', 'a')",
+        "open('filename.txt', 'x')"
+      ]
+    },
+  {
+    numb: 39,
+    question: "Which of the following is used to add a new key-value pair to a dictionary in Python?",
+    answer: "dictionary[key] = value",
+    options: [
+      "dictionary.add(key, value)",
+      "dictionary.insert(key, value)",
+      "dictionary.update(key, value)",
+      "dictionary[key] = value"
+    ]
+  },
+  {
+    numb: 40,
+    question: "What is the output of the following code? \nprint(len([i for i in range(0, 10, 2)]))",
+    answer: "5",
+    options: [
+      "10",
+      "5",
+      "9",
+      "4"
+    ]
+  },
+  {
+    numb: 41,
+    question: "Who was the first president of the United States?",
+    answer: "George Washington",
+    options: [
+      "Thomas Jefferson",
+      "Abraham Lincoln",
+      "George Washington",
+      "John F. Kennedy"
+    ]
+  },
+  {
+    numb: 42,
+    question: "What is the capital of Canada?",
+    answer: "Ottawa",
+    options: [
+      "Toronto",
+      "Montreal",
+      "Vancouver",
+      "Ottawa"
+    ]
+  },
+  {
+    numb: 43,
+    question: "Who is known as the 'Father of Modern Economics'?",
+    answer: "Adam Smith",
+    options: [
+      "Karl Marx",
+      "John Maynard Keynes",
+      "Adam Smith",
+      "Milton Friedman"
+    ]
+  },
+  {
+    numb: 44,
+    question: "Which country was the first to grant women the right to vote?",
+    answer: "New Zealand",
+    options: [
+      "United States",
+      "United Kingdom",
+      "New Zealand",
+      "Canada"
+    ]
+  },
+  {
+    numb: 45,
+    question: "What is the longest river in the world?",
+    answer: "Nile",
+    options: [
+      "Amazon",
+      "Yangtze",
+      "Mississippi",
+      "Nile"
+    ]
+  },
+  {
+    numb: 46,
+    question: "Who wrote the 'The Wealth of Nations'?",
+    answer: "Adam Smith",
+    options: [
+      "Karl Marx",
+      "Adam Smith",
+      "John Maynard Keynes",
+      "Milton Friedman"
+    ]
+  },
+  {
+    numb: 47,
+    question: "Which war is known as the 'Great War'?",
+    answer: "World War I",
+    options: [
+      "World War I",
+      "World War II",
+      "Cold War",
+      "Vietnam War"
+    ]
+  },
+  {
+    numb: 48,
+    question: "Which city is known as the 'City of Love'?",
+    answer: "Paris",
+    options: [
+      "Rome",
+      "Paris",
+      "Venice",
+      "Florence"
+    ]
+  },
+  {
+    numb: 49,
+    question: "Who was the first woman to win a Nobel Prize?",
+    answer: "Marie Curie",
+    options: [
+      "Mother Teresa",
+      "Marie Curie",
+      "Rosa Parks",
+      "Malala Yousafzai"
+    ]
+  },
+  {
+    numb: 50,
+    question: "What is the capital of Brazil?",
+    answer: "Brasília",
+    options: [
+      "Rio de Janeiro",
+      "São Paulo",
+      "Brasília",
+      "Salvador"
+    ]
+  },
+  {
+    numb: 51,
+    question: "Who wrote 'The Communist Manifesto'?",
+    answer: "Karl Marx and Friedrich Engels",
+    options: [
+      "Vladimir Lenin",
+      "Joseph Stalin",
+      "Karl Marx and Friedrich Engels",
+      "Leon Trotsky"
+    ]
+  },
+  {
+    numb: 52,
+    question: "Which country was the first to send an object into space?",
+    answer: "Soviet Union",
+    options: [
+      "United States",
+      "China",
+      "Soviet Union",
+      "France"
+    ]
+  },
+  {
+    numb: 53,
+    question: "What is the largest continent by land area?",
+    answer: "Asia",
+    options: [
+      "North America",
+      "Africa",
+      "Europe",
+      "Asia"
+    ]
+  },
+  {
+    numb: 54,
+    question: "Who was the first female Prime Minister of the United Kingdom?",
+    answer: "Margaret Thatcher",
+    options: [
+      "Theresa May",
+      "Margaret Thatcher",
+      "Angela Merkel",
+      "Jacinda Ardern"
+    ]
+  },
+  {
+    numb: 55,
+    question: "What is the official language of Japan?",
+    answer: "Japanese",
+    options: [
+      "Mandarin",
+      "Korean",
+      "Japanese",
+      "Cantonese"
+    ]
+  },
+  // General Knowledge
+  {
+    numb: 56,
+    question: "What is the capital city of Australia?",
+    answer: "Canberra",
+    options: [
+      "Sydney",
+      "Melbourne",
+      "Canberra",
+      "Perth"
+    ]
+  },
+  {
+    numb: 57,
+    question: "What is the capital of South Korea?",
+    answer: "Seoul",
+    options: [
+      "Busan",
+      "Incheon",
+      "Seoul",
+      "Daegu"
+    ]
+  },
+  {
+    numb: 58,
+    question: "Who painted the Mona Lisa?",
+    answer: "Leonardo da Vinci",
+    options: [
+      "Pablo Picasso",
+      "Vincent van Gogh",
+      "Leonardo da Vinci",
+      "Michelangelo"
+    ]
+  },
+  {
+    numb: 59,
+    question: "Which planet is known as the 'Red Planet'?",
+    answer: "Mars",
+    options: [
+      "Venus",
+      "Mars",
+      "Jupiter",
+      "Saturn"
+    ]
+  },
+  {
+    numb: 60,
+    question: "Who is the author of 'To Kill a Mockingbird'?",
+    answer: "Harper Lee",
+    options: [
+      "J.K. Rowling",
+      "Harper Lee",
+      "Mark Twain",
+      "Charles Dickens"
+    ]
+  },
+  {
+    numb: 61,
+    question: "What is the chemical symbol for silver?",
+    answer: "Ag",
+    options: [
+      "S",
+      "Si",
+      "Ag",
+      "Au"
+    ]
+  },
+];
